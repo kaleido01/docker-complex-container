fd2f0d6eb69538e9ec49a82a5dbc2334e8f1332a
kaleido01 <mbdkaleido@gmail.com>
2019-10-08 08:14:41 +0900

    fix typo

--- server/keys.js
+++ server/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
   redisHost: process.env.REDIS_HOST,
   redisPort: process.env.REDIS_PORT,
-  pgUser: process.env.PRUSER,
-  pgHost: process.env.PRHOST,
-  pgUDatabase: process.env.PRDATABASE,
-  pgPassword: process.env.PRPASSWORD,
-  pgPort: process.env.PRPORT
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgUDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT
 };
