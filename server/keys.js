0fc4e86b5441f4e2b12586a9a944080bf6c346af
kaleido01 <mbdkaleido@gmail.com>
2019-10-07 20:55:17 +0900

    create server folder

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PRUSER,
+  pgHost: process.env.PRHOST,
+  pgUDatabase: process.env.PRDATABASE,
+  pgPassword: process.env.PRPASSWORD,
+  pgPort: process.env.PRPORT
+};
