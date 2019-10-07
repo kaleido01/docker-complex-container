cdaf56ca49eb534ca14c974f9a8cbf6883453161
kaleido01 <mbdkaleido@gmail.com>
2019-10-07 21:43:58 +0900

    add server code

--- server/index.js
+++ server/index.js
@@ -25,3 +25,42 @@ pgClient.on("error", () => console.log("Lost PG connection"));
 pgClient
   .query("CREATE TABLE IF NOT EXISTS values (number INT)")
   .catch(err => console.log(err));
+
+const redis = require("redis");
+const redisClient = redis.createClient({
+  port: keys.redisPort,
+  host: keys.redisHost,
+  retry_strategy: () => 1000
+});
+
+const redisPublisher = redisClient.duplicate();
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
+app.get("/values/all", async (req, res) => {
+  const values = await pgClient.query("SELECY * from values");
+  res.send(values.rows);
+});
+app.get("/values/current", async (req, res) => {
+  redisClient.hgetall("values", (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post("/values", async (req, res) => {
+  const index = req.body.index;
+  if (parseInt(index) > 40) {
+    return res.status(422).send("Index too high");
+  }
+  redisClient.hset("values", index, "Nothing Yet");
+  redisPublisher.publish("insert", index);
+  pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
+
+  res.send({ working: true });
+});
+
+app.listen(5000, () => {
+  console.log("Listening");
+});
